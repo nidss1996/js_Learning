981b82a774cedc35ac6ed2c4727e08d2d216e7c5
nidss1996 <97432716+nidss1996@users.noreply.github.com>
2023-11-28 13:13:41 +0000

    StringOperation

---
+++ 01_Basic/05_StringOperation.js
@@ -0,0 +1,16 @@
+const name = "Nidhi"
+console.log(name.charAt(4))
+const repoCount = 5
+
+// console.log( name + "repoCount value is = " + repoCount) // Old way 
+
+// console.log(`Hello my name is ${name} and total Repos count is ${repoCount}`) // strin interplotation 
+const Name = new String("Nidhi") // creating String object 
+// console.log(Name)
+
+// console.log(Name[0])
+// console.log(Name.__proto__)
+const st1 = "    Name     "
+// console.log(Name.charAt(3))
+// console.log(Name.indexOf('h'))
+// console.log(st1.trim())
