9a2aca9d1367972cc0172241f835761be083c299
nidss1996 <97432716+nidss1996@users.noreply.github.com>
2023-11-28 07:29:05 +0000

    DataTypes

---
+++ 01_Basic/03_Datatype.js
@@ -0,0 +1,15 @@
+let Name = "nidhi"
+let id = 101
+let accountDetails;
+// number => 2 to power 53 
+// bigInt 
+// string => "" , ''
+// boolean => true/fales
+// null => standalone value 
+// undefined 
+//symbol => for unique value 
+
+console.log(typeof Name)
+console.log(typeof accountDetails) // type is undefined 
+console.log(typeof null) // type is Object
+
