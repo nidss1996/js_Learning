e3e93fb2ddcea2c488ba5fc95fcd908f1bc166ad
nidss1996 <97432716+nidss1996@users.noreply.github.com>
2023-11-28 12:38:06 +0000

    Summary

--- 01_Basic/DataTypeSumarry.js
+++ 01_Basic/DataTypeSumarry.js
@@ -26,7 +26,34 @@ const myFunction = function()  {
     console.log("Hello World");
     
 }
-console.log(typeof myFunction)
+// console.log(typeof myFunction)
 
 // why we are using const in Array and Function 
 
+// ########### storing data in stack or Heap 
+
+// Primitive datatype store data in stack as a copy of value 
+
+let myName = " Nidhi "
+let myName1 = myName 
+
+// console.log(myName1)
+
+myName1 = " Komal "
+// console.log(myName1) // Komal 
+// console.log(myName) // Nidhi 
+
+// non Primitive data store data in Heap and it uses referance of memory 
+
+let Student1 = {
+    Name:"Nidhi",
+    email: " nidhi123@gmail.com",
+
+};
+
+let Student2 = Student1 
+console.log(Student2)
+Student2.email = "Jyothi123@gmail.com" // beacuse it is using the same referance so email 
+///Id will change in both the object Student1 and Student2  
+console.log(Student1)
+console.log(Student2)
