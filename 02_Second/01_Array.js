fb05cd35f06870de84b8f3efe5dfd5c96827fd65
nidss1996 <97432716+nidss1996@users.noreply.github.com>
2023-11-29 08:26:13 +0000

    Array Full part

---
+++ 02_Second/01_Array.js
@@ -0,0 +1,22 @@
+//console.log("Hello Array") 
+//JavaScript array-copy operations create shallow copies(share or point same memory reference )
+const ar1 = [1, 3, ,44 ,6, 7]
+const ar2 = ["a", "b", "cAdudi"]
+const array1 = new Array(1, 3, 4, 5, "Nidhi")
+//const array2 = array1
+//
+//array2.push(101)
+//console.log(array2) 
+//console.log(array1)
+//console.log(array1[4])
+//Methods on Array 
+//console.log(array1.push(89))
+//console.log(array1.push(98))
+//console.log(array1.pop())
+//array1.unshift(10)
+//console.log(array1) // add the value at first place 
+array1.shift() // remove the first value from starting 
+array1.shift()
+console.log(array1)
+//array1.shift()
+//console.log(array1)
