54418713d26fb97d647f280e0d3fc1c015ab0149
nidss1996 <97432716+nidss1996@users.noreply.github.com>
2023-11-30 10:30:30 +0000

    Final of Function

--- 02_Second/03_Function.js
+++ 02_Second/03_Function.js
@@ -35,5 +35,38 @@ function userLoggedMessage(username= "Nidhi"){
 }
  const result = userLoggedMessage()
  
- console.log(result)
+ //console.log(result)
  //console.log(typeof username)
+
+ // Rest Operator in function (rest operator is used to add dynamic argument in a function)
+ function addProduct(val1, val2,...num) { 
+    return num
+
+ }
+ console.log(addProduct(100, 200, 1 , 300))
+
+ // pass objeect in function 
+ const user  = {
+    username: "Nidhi",
+    price:900
+ }
+
+ function handleObject(anyobject){
+    return `userName is ${anyobject.username} and price is ${anyobject.price}`
+
+ }
+
+ //console.log(handleObject(user))
+ /*console.log(handleObject({
+    username:"Nidhi",
+    price:400
+ })) */
+
+ // pass array in a function 
+
+ const ar1 = [12, 14, 1,5 ,16]
+ function handleArray(getArray){
+    return getArray[3]
+ }
+ console.log(handleArray(ar1))
+
