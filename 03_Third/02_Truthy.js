38dc5341a9ebd12e121b870bb565562b5f157a74
nidss1996 <97432716+nidss1996@users.noreply.github.com>
2023-12-01 16:20:59 +0000

    Nullish Coalescing Operator (??)

--- 03_Third/02_Truthy.js
+++ 03_Third/02_Truthy.js
@@ -1,10 +1,42 @@
-const userEmailId = "nidhi123@gmail.com"
-if(userEmailId){
-    console.log("got email ")
-}
-else{
-    console.log("not found")
-}
+const userEmailId = []
+// if(userEmailId){
+//     console.log("got email ")
+// }
+// else{
+//     console.log("not found")
+// }
 
 //falsy value 
-false, 0, -0, BigInt 0n, "", null
+//false, 0, -0, BigInt 0n, "", null, undefined , NaN
+//truthy value : "0", 'fales', " ", [],{}, function(){}
+
+// if(userEmailId.length === 0){
+//     console.log("Array is empty ")
+
+// }
+
+// const user = {}
+
+
+// if(Object.keys(user).length === 0){
+//     console.log("Array is empty ")
+
+// }
+
+// $$$$$$$$ Nullish Coalescing Operators (??) : works on null, undefined
+
+// val1 = 5 ?? 10 
+// console.log(val1)
+// val2 = null ?? 10 
+// console.log(val2)
+// val3 = undefined ?? 30 
+// console.log(val3)
+// val4 = undefined ?? 30 ?? 50 ?? 60
+// console.log(val4)
+
+// Ternaiary Operator 
+// condition ? true statement : fales statement 
+
+// let iceCreamPrice = 100 
+// iceCreamPrice <=80 ? console.log("price less than 80") : console.log("Price more than 80")
+
