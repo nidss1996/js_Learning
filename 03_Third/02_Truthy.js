3c1b25965e8b9248c8b5937cb723c8733a79c338
nidss1996 <97432716+nidss1996@users.noreply.github.com>
2023-12-01 15:52:23 +0000

    comit

---
+++ 03_Third/02_Truthy.js
@@ -0,0 +1,10 @@
+const userEmailId = "nidhi123@gmail.com"
+if(userEmailId){
+    console.log("got email ")
+}
+else{
+    console.log("not found")
+}
+
+//falsy value 
+false, 0, -0, BigInt 0n, "", null
