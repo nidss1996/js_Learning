112105803de528909a45551c3ba9f4946545aca1
nidss1996 <97432716+nidss1996@users.noreply.github.com>
2023-12-01 16:52:57 +0000

    loops

---
+++ 03_Third/03_Loops.js
@@ -0,0 +1,24 @@
+// for (let index =1; index <= 10; index++) {
+//     console.log(`Outer Loop ${index}`)
+//     for (let j = 1; j <=10; j++) {
+//         //console.log(`Inner loop ${j} and outer loop value ${index}`)
+//         console.log(`${index} * ${j} = ` + index * j)
+        
+//     } 
+// }
+
+// const myArray = ["Nidhi", "Kshitij", "Tanu"]
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element)
+    
+// }
+
+for (let i = 0; i < 10; i++) {
+   if(i == 6)
+   {
+    //console.log(`${i} detected `)
+    continue
+   }
+    console.log(i)
+}
