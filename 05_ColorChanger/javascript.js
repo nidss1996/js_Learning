a60387e9faaf058d386756875c5b8df318b5c9fc
nidss1996 <97432716+nidss1996@users.noreply.github.com>
2023-12-07 13:37:55 +0000

    done

---
+++ 05_ColorChanger/javascript.js
@@ -0,0 +1,31 @@
+ const button = document.querySelectorAll('.button')
+//console.log(button)
+button.forEach(function(btn){
+    btn.addEventListener('click', function(e){
+        //console.log("nidhi")
+        //console.log(e)
+        console.log(e.target)
+        if(e.target.id === 'grey')
+        {
+           document.body.style.backgroundColor = e.target.id
+        }
+
+        if(e.target.id === 'yellow')
+        {
+           document.body.style.backgroundColor = e.target.id
+        }
+        if(e.target.id === 'blue')
+        {
+           document.body.style.backgroundColor = e.target.id
+        }
+        if(e.target.id === 'white')
+        {
+           document.body.style.backgroundColor = e.target.id
+        }
+
+    })
+
+});
+    
+
+
