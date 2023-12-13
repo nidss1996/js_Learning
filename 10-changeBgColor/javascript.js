b3eeee5dab9fa74f8dda5a392ad5c7272b671fee
nidss1996 <97432716+nidss1996@users.noreply.github.com>
2023-12-13 20:02:01 +0000

    final touch

--- 10-changeBgColor/javascript.js
+++ 10-changeBgColor/javascript.js
@@ -8,21 +8,34 @@ function getRandomColor(){
     console.log(color)  
     return color 
 }
-  const stratChangeColor = function(){
-        color = '#'
-       let hashVal = getRandomColor()
-       document.body.style.backgroundColor = `${hashVal}`
-  }
-  
-        const sayDate = function(str){
-           console.log(str, Date.now())
-        }
-      // 
-       const start = document.querySelector('#start').addEventListener('click',function(e){
-         intervalId=  setInterval(stratChangeColor,2000,"hey")
-       })
 
-       const stop = document.querySelector('#stop').addEventListener('click',function(e){
-        clearInterval(intervalId)
-        console.log("no data")
-       })
+const startchangeBgColor = function(){
+  const stratChangeColor = function(){
+    color = '#'
+   let hashVal = getRandomColor()
+   document.body.style.backgroundColor = `${hashVal}`
+}
+ if(!intervalId ){
+  intervalId=  setInterval(stratChangeColor,2000)
+ }
+  
+  
+};
+
+const stopchangBgColor = function(){
+
+  clearInterval(intervalId)
+  intervalId = null // default meaning is fales
+  console.log(intervalId)
+};
+        // const sayDate = function(str){
+        //    console.log(str, Date.now())
+        // }
+      // 
+       const start = document.querySelector('#start').addEventListener('click',startchangeBgColor)
+         //intervalId=  setInterval(stratChangeColor,2000)
+       
+
+       const stop = document.querySelector('#stop').addEventListener('click',stopchangBgColor)
+        
+        
